bcf202dd40fe3d469bd69ba6142350d1ece22787
shigek-i <shigeki.name@gmail.com>
2022-09-14 06:23:16 +0900

    added initializer

--- Sources/SwiftUIDynamicPageView/PageView.swift
+++ Sources/SwiftUIDynamicPageView/PageView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 public struct PageView<Selection: Hashable & Comparable, Content: View>: View {
-    @Binding var selection: Selection
-
-    let preparePreviousSelection: (Selection) -> Selection
-    let prepareNextSelection: (Selection) -> Selection
-    let onPageWillChange: ((Selection) -> Void)?
-    let onPageChanged: ((Selection) -> Void)?
-
-    let content: (Selection) -> Content
+    @Binding private var selection: Selection
+    
+    private let preparePreviousSelection: (Selection) -> Selection
+    private let prepareNextSelection: (Selection) -> Selection
+    private let onPageWillChange: ((Selection) -> Void)?
+    private let onPageChanged: ((Selection) -> Void)?
+    
+    private let content: (Selection) -> Content
+    
+    init(
+        selection: Binding<Selection>,
+        preparePreviousSelection: @escaping (Selection) -> Selection,
+        prepareNextSelection: @escaping (Selection) -> Selection,
+        onPageWillChange: ((Selection) -> Void)? = nil,
+        onPageChanged: ((Selection) -> Void)? = nil,
+        @ViewBuilder content: @escaping (Selection) -> Content
+    ) {
+        self._selection = selection
+        self.preparePreviousSelection = preparePreviousSelection
+        self.prepareNextSelection = prepareNextSelection
+        self.onPageChanged = onPageChanged
+        self.onPageWillChange = onPageWillChange
+        self.content = content
+    }
 
     public var body: some View {
         DynamicPageView(
